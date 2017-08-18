d9142655ed4aedb92b9bcb789a1ec96a22a4cd3b
James <jdjarrell_3@yahoo.com>
2017-08-17 22:41:34 -0700

    This should update my GitHub to what I've got as of 2241 on 17Aug

---
+++ projects/jarrellj/HW04/Calendar/src/test/java/calendar/ApptRandomTest.java
@@ -0,0 +1,395 @@
+package calendar;
+
+import java.util.Calendar;
+import java.util.Random;
+
+import org.junit.Test;
+
+
+
+import static org.junit.Assert.*;
+
+
+
+/**
+ * Random Test Generator  for Appt class.
+ */
+
+public class ApptRandomTest {
+	private static final long TestTimeout = 60 * 500 * 1; /* Timeout at 30 seconds */
+	private static final int NUM_TESTS=100;
+
+	/**
+	 * Return a randomly selected method to be tests !.
+	 */
+    public static String RandomSelectMethod(Random random){
+        String[] methodArray = new String[] {"setTitle","setRecurrence"};// The list of the of methods to be tested in the Appt class
+
+    	int n = random.nextInt(methodArray.length);// get a random number between 0 (inclusive) and  methodArray.length (exclusive)
+    	            
+        return methodArray[n] ; // return the method name 
+        }
+	/**
+	 * Return a randomly selected appointments to recur Weekly,Monthly, or Yearly !.
+	 */
+    public static int RandomSelectRecur(Random random){
+        int[] RecurArray = new int[] {Appt.RECUR_BY_WEEKLY,Appt.RECUR_BY_MONTHLY,Appt.RECUR_BY_YEARLY};// The list of the of setting appointments to recur Weekly,Monthly, or Yearly
+
+    	int n = random.nextInt(RecurArray.length);// get a random number between 0 (inclusive) and  RecurArray.length (exclusive)
+        return RecurArray[n] ; // return the value of the  appointments to recur 
+        }	
+	/**
+	 * Return a randomly selected appointments to recur forever or Never recur  !.
+	 */
+    public static int RandomSelectRecurForEverNever(Random random){
+        int[] RecurArray = new int[] {Appt.RECUR_NUMBER_FOREVER,Appt.RECUR_NUMBER_NEVER};// The list of the of setting appointments to recur RECUR_NUMBER_FOREVER, or RECUR_NUMBER_NEVER
+
+    	int n = random.nextInt(RecurArray.length);// get a random number between 0 (inclusive) and  RecurArray.length (exclusive)
+        return RecurArray[n] ; // return appointments to recur forever or Never recur 
+        }	
+   /**
+     * Generate Random Tests that tests Appt Class.
+     */
+	 @Test
+	  public void radnomtest()  throws Throwable  {
+
+		 long startTime = Calendar.getInstance().getTimeInMillis();
+		 long elapsed = Calendar.getInstance().getTimeInMillis() - startTime;
+
+		 
+		 System.out.println("Start testing...");
+		 
+		try{ 
+			for (int iteration = 0; elapsed < TestTimeout; iteration++) {
+				long randomseed =System.currentTimeMillis(); //10
+	//			System.out.println(" Seed:"+randomseed );
+				Random random = new Random(randomseed);
+				
+				 int startHour=ValuesGenerator.RandInt(random);
+				 int startMinute=ValuesGenerator.RandInt(random);
+				 int startDay=ValuesGenerator.RandInt(random);;
+				 int startMonth=ValuesGenerator.RandInt(random);;
+				 int startYear=ValuesGenerator.RandInt(random);
+				 String title="Birthday Party";
+				 String description="This is my birthday party.";
+				 //Construct a new Appointment object with the initial data	 
+				 Appt appt = new Appt(startHour,
+				          startMinute ,
+				          startDay ,
+				          startMonth ,
+				          startYear ,
+				          title,
+				         description);
+			 if(!appt.getValid())continue;
+			for (int i = 0; i < NUM_TESTS; i++) {
+					String methodName = ApptRandomTest.RandomSelectMethod(random);
+					   if (methodName.equals("setTitle")){
+						   String newTitle=(String) ValuesGenerator.getString(random);
+						   appt.setTitle(newTitle);						   
+						}
+					   else if (methodName.equals("setRecurrence")){
+						   int sizeArray=ValuesGenerator.getRandomIntBetween(random, 0, 8);
+						   int[] recurDays=ValuesGenerator.generateRandomArray(random, sizeArray);
+						   int recur=ApptRandomTest.RandomSelectRecur(random);
+						   int recurIncrement = ValuesGenerator.RandInt(random);
+						   int recurNumber=ApptRandomTest.RandomSelectRecurForEverNever(random);
+						   appt.setRecurrence(recurDays, recur, recurIncrement, recurNumber);
+						}				
+				}
+				
+				 elapsed = (Calendar.getInstance().getTimeInMillis() - startTime);
+			        if((iteration%10000)==0 && iteration!=0 )
+			              System.out.println("elapsed time: "+ elapsed + " of "+TestTimeout);
+			 
+			}
+		}catch(NullPointerException e){
+			
+		}
+
+		 System.out.println("Done testing...");
+	 }
+
+
+/*	 @Test
+//Tests all true of nested if statements; lines 113 to 126
+	  public void test01()  throws Throwable  {
+		 int startHour=13;
+		 int startMinute=30;
+		 int startDay=10;
+		 int startMonth=4;
+		 int startYear=2017;
+		 String title="Birthday Party";
+		 String description="This is my birthday party.";
+		 //Construct a new Appointment object with the initial data	 
+		 Appt appt = new Appt(startHour,
+		          startMinute ,
+		          startDay ,
+		          startMonth ,
+		          startYear ,
+		          title,
+		         description);
+		//Tests all setStart_____ functions
+		 appt.setStartHour(14);
+		 appt.setStartMinute(45);
+		 appt.setStartDay(18);
+		 appt.setStartMonth(7);
+		 appt.setStartYear(2525);
+		 appt.setTitle(null);
+		 appt.setTitle("The Future");
+		 appt.setDescription(null);
+		 appt.setDescription("It's dangerous, sexy, and now");
+	// assertions
+		 assertTrue(appt.getValid());
+		 assertEquals(14, appt.getStartHour());
+		 assertEquals(45, appt.getStartMinute());
+		 assertEquals(18, appt.getStartDay());
+		 assertEquals(07, appt.getStartMonth());
+		 assertEquals(2525, appt.getStartYear());
+		 assertEquals("The Future", appt.getTitle());
+		 assertEquals("It's dangerous, sexy, and now", appt.getDescription());         		
+		 appt.getRecurNumber();
+		 appt.getRecurBy();
+		 appt.getRecurDays();
+		 appt.isRecurring();
+		 appt.getRecurIncrement();
+	 }
+	 @Test
+//Tests high fail of line 113 in Appt.java
+	  public void test02()  throws Throwable  {
+		 int startHour=54;
+		 int startMinute=30;
+		 int startDay=10;
+		 int startMonth=4;
+		 int startYear=2017;
+		 String title="Birthday Party";
+		 String description="This is my birthday party.";
+		 //Construct a new Appointment object with the initial data	 
+		 Appt appt = new Appt(startHour,
+		          startMinute ,
+		          startDay ,
+		          startMonth ,
+		          startYear ,
+		          title,
+		         description);
+	// assertions
+		 assertTrue(!appt.getValid());
+		 assertEquals(54, appt.getStartHour());
+		 assertEquals(30, appt.getStartMinute());
+		 assertEquals(10, appt.getStartDay());
+		 assertEquals(04, appt.getStartMonth());
+		 assertEquals(2017, appt.getStartYear());
+		 assertEquals("Birthday Party", appt.getTitle());
+		 assertEquals("This is my birthday party.", appt.getDescription());         		
+	 
+	 }
+	 @Test
+//Tests low fail of line 113 in Appt.java
+	  public void test03()  throws Throwable  {
+		 int startHour=-10;
+		 int startMinute=30;
+		 int startDay=10;
+		 int startMonth=4;
+		 int startYear=2017;
+		 String title="Birthday Party";
+		 String description="This is my birthday party.";
+		 //Construct a new Appointment object with the initial data	 
+		 Appt appt = new Appt(startHour,
+		          startMinute ,
+		          startDay ,
+		          startMonth ,
+		          startYear ,
+		          title,
+		         description);
+	// assertions
+		 assertTrue(!appt.getValid());
+		 assertEquals(-10, appt.getStartHour());
+		 assertEquals(30, appt.getStartMinute());
+		 assertEquals(10, appt.getStartDay());
+		 assertEquals(04, appt.getStartMonth());
+		 assertEquals(2017, appt.getStartYear());
+		 assertEquals("Birthday Party", appt.getTitle());
+		 assertEquals("This is my birthday party.", appt.getDescription());         		
+	 
+	 }
+	 @Test
+//Tests high fail of line 116 in Appt.java
+	  public void test04()  throws Throwable  {
+		 int startHour=12;
+		 int startMinute=75;
+		 int startDay=10;
+		 int startMonth=4;
+		 int startYear=2017;
+		 String title="Birthday Party";
+		 String description="This is my birthday party.";
+		 //Construct a new Appointment object with the initial data	 
+		 Appt appt = new Appt(startHour,
+		          startMinute ,
+		          startDay ,
+		          startMonth ,
+		          startYear ,
+		          title,
+		         description);
+	// assertions
+		 assertTrue(!appt.getValid());
+		 assertEquals(12, appt.getStartHour());
+		 assertEquals(75, appt.getStartMinute());
+		 assertEquals(10, appt.getStartDay());
+		 assertEquals(04, appt.getStartMonth());
+		 assertEquals(2017, appt.getStartYear());
+		 assertEquals("Birthday Party", appt.getTitle());
+		 assertEquals("This is my birthday party.", appt.getDescription());         		
+	 
+	 }
+	 @Test
+//Tests low fail of line 116 in Appt.java
+	  public void test05()  throws Throwable  {
+		 int startHour=12;
+		 int startMinute=-30;
+		 int startDay=10;
+		 int startMonth=4;
+		 int startYear=2017;
+		 String title="Birthday Party";
+		 String description="This is my birthday party.";
+		 //Construct a new Appointment object with the initial data	 
+		 Appt appt = new Appt(startHour,
+		          startMinute ,
+		          startDay ,
+		          startMonth ,
+		          startYear ,
+		          title,
+		         description);
+	// assertions
+		 assertTrue(!appt.getValid());
+		 assertEquals(12, appt.getStartHour());
+		 assertEquals(-30, appt.getStartMinute());
+		 assertEquals(10, appt.getStartDay());
+		 assertEquals(04, appt.getStartMonth());
+		 assertEquals(2017, appt.getStartYear());
+		 assertEquals("Birthday Party", appt.getTitle());
+		 assertEquals("This is my birthday party.", appt.getDescription());         		
+	 
+	 }
+	 @Test
+//Tests high fail of line 119 in Appt.java
+	  public void test06()  throws Throwable  {
+		 int startHour=12;
+		 int startMinute=30;
+		 int startDay=100;
+		 int startMonth=4;
+		 int startYear=2017;
+		 String title="Birthday Party";
+		 String description="This is my birthday party.";
+		 //Construct a new Appointment object with the initial data	 
+		 Appt appt = new Appt(startHour,
+		          startMinute ,
+		          startDay ,
+		          startMonth ,
+		          startYear ,
+		          title,
+		         description);
+	// assertions
+		 assertTrue(!appt.getValid());
+		 assertEquals(12, appt.getStartHour());
+		 assertEquals(30, appt.getStartMinute());
+		 assertEquals(100, appt.getStartDay());
+		 assertEquals(04, appt.getStartMonth());
+		 assertEquals(2017, appt.getStartYear());
+		 assertEquals("Birthday Party", appt.getTitle());
+		 assertEquals("This is my birthday party.", appt.getDescription());         		
+	 
+	 }
+	 @Test
+//Tests low fail of line 119 in Appt.java
+	  public void test07()  throws Throwable  {
+		 int startHour=12;
+		 int startMinute=30;
+		 int startDay=-10;
+		 int startMonth=4;
+		 int startYear=2017;
+		 String title="Birthday Party";
+		 String description="This is my birthday party.";
+		 //Construct a new Appointment object with the initial data	 
+		 Appt appt = new Appt(startHour,
+		          startMinute ,
+		          startDay ,
+		          startMonth ,
+		          startYear ,
+		          title,
+		         description);
+	// assertions
+		 assertTrue(!appt.getValid());
+		 assertEquals(12, appt.getStartHour());
+		 assertEquals(30, appt.getStartMinute());
+		 assertEquals(-10, appt.getStartDay());
+		 assertEquals(04, appt.getStartMonth());
+		 assertEquals(2017, appt.getStartYear());
+		 assertEquals("Birthday Party", appt.getTitle());
+		 assertEquals("This is my birthday party.", appt.getDescription());         		
+	 
+	 }
+	 @Test
+//Tests high fail of line 122 in Appt.java
+	  public void test08()  throws Throwable  {
+		 int startHour=12;
+		 int startMinute=30;
+		 int startDay=10;
+		 int startMonth=15;
+		 int startYear=2017;
+		 String title="Birthday Party";
+		 String description="This is my birthday party.";
+		 //Construct a new Appointment object with the initial data	 
+		 Appt appt = new Appt(startHour,
+		          startMinute ,
+		          startDay ,
+		          startMonth ,
+		          startYear ,
+		          title,
+		         description);
+	// assertions
+		 assertTrue(!appt.getValid());
+		 assertEquals(12, appt.getStartHour());
+		 assertEquals(30, appt.getStartMinute());
+		 assertEquals(10, appt.getStartDay());
+		 assertEquals(15, appt.getStartMonth());
+		 assertEquals(2017, appt.getStartYear());
+		 assertEquals("Birthday Party", appt.getTitle());
+		 assertEquals("This is my birthday party.", appt.getDescription());         		
+	 
+	 }
+	 @Test
+//Tests low fail of line 122 in Appt.java
+	  public void test09()  throws Throwable  {
+		 int startHour=12;
+		 int startMinute=30;
+		 int startDay=10;
+		 int startMonth=0;
+		 int startYear=2017;
+		 String title="Birthday Party";
+		 String description="This is my birthday party.";
+		 //Construct a new Appointment object with the initial data	 
+		 Appt appt = new Appt(startHour,
+		          startMinute ,
+		          startDay ,
+		          startMonth ,
+		          startYear ,
+		          title,
+		         description);
+	// assertions
+		 assertTrue(!appt.getValid());
+		 assertEquals(12, appt.getStartHour());
+		 assertEquals(30, appt.getStartMinute());
+		 assertEquals(10, appt.getStartDay());
+		 assertEquals(00, appt.getStartMonth());
+		 assertEquals(2017, appt.getStartYear());
+		 assertEquals("Birthday Party", appt.getTitle());
+		 assertEquals("This is my birthday party.", appt.getDescription());         		
+	 
+	 }
+
+	 
+//		 System.out.println("Done testing...");
+//	 }
+*/
+
+	
+}
